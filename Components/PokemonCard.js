568641779c08590d3dc936cca580edc738ae1036
fausio luis <fausioluis@live.com>
2025-05-26 13:14:42 +0200

    style.card

--- Components/PokemonCard.js
+++ Components/PokemonCard.js
@@ -1,10 +1,32 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet, Platform } from "react-native";
+
+
+const style = StyleSheet.create({
+    card: {
+        backgroundColor: "white",
+        borderRadius: 16,
+        borderWidth: 2,
+        padding: 16,
+        margin: 16,
+        ...Platform.select({
+            ios: {
+                shadowOffset: { width: 2, height: 2 },
+                shadowColor: "#333",
+                shadowOpacity: 0.3,
+                shadowRadius: 4,
+            },
+            android: {
+                elevation: 5,
+            },
+        }),
+    },
+})
 
 
 export default function PokemonCard() {
 
     return (
-        <View>
+        <View style={style.card}>
             <Text>Pokemon Card</Text>
         </View>
     )
